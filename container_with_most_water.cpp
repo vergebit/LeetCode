1795c85b00970bc12e6c2a71a0baa73c962f6a0b
vergebit <vergebit@gmail.com>
2021-05-27 14:32:44 +0530

    Adding container_with_most_water.cpp

---
+++ container_with_most_water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int max_area = 0;
+        int curr_area = 0;
+        int beg = 0;
+        int end = height.size()-1;
+
+        while(beg<end){
+            curr_area = min(height[beg], height[end])*(end-beg);
+            max_area = max(max_area, curr_area);
+            
+            if(height[beg]<=height[end]){
+                beg++;
+            }
+            else{
+                end--;
+            }
+        }
+        return max_area;
+    }
+};
